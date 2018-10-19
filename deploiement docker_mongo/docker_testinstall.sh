9f53be9c1c8841919755d10d72602fc3b369130f
Alexandre LOËNS <a.loens@intech-sud.fr>
2018-10-19 17:01:14 +0200

    Function edited and tested
    Function is operational now (some tweaks have been made)

--- deploiement docker_mongo/docker_testinstall.sh
+++ deploiement docker_mongo/docker_testinstall.sh
@@ -2,15 +2,19 @@
 #Run aftel install verification for docker
 #################
 	docker version
-	if [$? != 0 ]
+	if [ $? -ne 0 ]
 		then
 #Error 4: Unable to recover docker version, install may have failed
 			exit 4
 	fi
-	docker run hello-world
-	if [$? != 0]
+	docker run --name hello-world hello-world
+	if [ $? -ne 0 ]
 		then
 #Error 5: Unable tu start container hello-world, connection to Docker Hub may have failed
+			docker ps -a
+			docker stop hello-world && docker rm hello-world
 			exit 5
 	fi
+		docker ps -a
+		docker stop hello-world && docker rm hello-world
 	exit $?
