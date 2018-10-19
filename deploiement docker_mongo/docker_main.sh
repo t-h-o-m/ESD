1b5edf523b11b280cfe537232c91ae2911ab80b2
Alexandre LOËNS <a.loens@intech-sud.fr>
2018-10-19 15:26:05 +0200

    Creation of docker_main.sh
    It's the main script, that has to be run as root and then does everything else and go back to user privileges when root isn't needed anymore

--- deploiement docker_mongo/docker_main.sh
+++ deploiement docker_mongo/docker_main.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #This file is the index of all functions for privileges allocaiton, docker engine installation and docker deployment on a debian 9 system
 #################
-
-
+##SCRIPT COMMAND USAGE : bash docker_main.sh {argument1} {argument2} {argument3} {argumentn}
+#Arguments check
+$d_image=$1
+if [ $d_image = "" ]
+then $d_image=mongo
+fi
 
 # Exit if the script was not launched by root
 if [ $USER != "root" ]
@@ -11,6 +15,8 @@ then
 fi
 ## Run the job that needs to be run as root
 #For instance : command arguments
-
+bash docker_apt.sh sudo
+bash docker_createuser.sh docker
 ## Run the job(s) that don't need root
 #For instance : su user -c "command arguments"
+su docker -c "bash docker_mongoDB_deployment.sh mongodb 27017 27017 $d_image"
