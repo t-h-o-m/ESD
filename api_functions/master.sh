a3db29b6f7ca43e58562b952c6a146950a166cf8
Vincent Matias <v.matias@intech-sud.fr>
2018-11-08 14:44:07 +0100

    Fully operational scripts for Release 1

--- api_functions/master.sh
+++ api_functions/master.sh
@@ -1,15 +1,11 @@
-#/bin/bash
+#!/bin/bash
 
-#Lancement du script d'instanciation de VM 
 
-#python create_instance.py
-python ./create_instance.py -pn unautrenom -gid None -fid 9b312f04-e6ff-493f-bbd1-134510f49258 -mbill False -iid 07b741aa-3a50-45f4-8841-9692eea29428 -rid SBG5 --sshkey 63334e6f63325679646d5679 > resultat.txt
+sudo python ./create_instance.py -pn MongoCloud1 -gid None -fid 9b312f04-e6ff-493f-bbd1-134510f49258 -mbill False -iid 07b741aa-3a50-45f4-8841-9692eea29428 -rid SBG5 --sshkey 63334e6f63325679646d5679 > resultat.json
 
-ip = echo `cat resultat.txt`
- 
-#Installation VM 
+instance_ip=`cat resultat.json`
 
-sudo ssh debian@$ip 'sudo bash -s ' < docker_allinone.sh
+sudo ssh -i /agilitation/id_rsa debian@$instance_ip 'sudo bash -s ' < docker_allinone.sh
 
 
 
--- api_functions/parse_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('resultat.json') as f:
-	json_content = json.load(f)
-
-print(json_content['id'])
